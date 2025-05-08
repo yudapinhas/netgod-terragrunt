293563df75f9f76a4b3d2bacda9fee69d37ddd7b
NetGod <yuda.pinhas@skai.io>
2025-05-08 17:30:03 +0200

    Update pull_request.groovy

--- buildScripts/jenkins/dsl/pull_request.groovy
+++ buildScripts/jenkins/dsl/pull_request.groovy
@@ -1,39 +1,17 @@
 pipelineJob('netgod-terraform-pull-request') {
-    def repo = 'https://github.com/yudapinhas/netgod-terraform'
-    def sshRepo = 'git@github.com:yudapinhas/netgod-terraform.git'
-
-    logRotator {
-        numToKeep(30)
-    }
-
     description("Pull Request CI for netgod-terraform")
-    properties {
-        githubProjectUrl(repo)
-    }
-
     definition {
         cpsScm {
             scm {
                 git {
                     remote {
-                        url(sshRepo)
+                        url('git@github.com:yudapinhas/netgod-terraform.git')
                         credentials('github-ssh-key')
                         refspec('+refs/pull/*:refs/remotes/origin/pr/*')
                     }
                     branches('*/master')
                 }
                 scriptPath('buildScripts/jenkins/pipeline/pull_request.groovy')
-            }
-        }
-    }
-
-    triggers {
-        githubPullRequest {
-            useGitHubHooks()
-            extensions {
-                commitStatus {
-                    context("CI - netgod-terraform pull request")
-                }
             }
         }
     }
