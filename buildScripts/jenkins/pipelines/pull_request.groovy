ca6d36c08da62fdd9c9f065bef4f2ef82c2db7fb
NetGod <yuda.pinhas@skai.io>
2025-05-08 22:25:18 +0200

    Update pull_request.groovy

--- buildScripts/jenkins/pipelines/pull_request.groovy
+++ buildScripts/jenkins/pipelines/pull_request.groovy
@@ -4,10 +4,6 @@ pipeline {
     agent any
 
     environment {
-        PATH = "${env.WORKSPACE}:${env.HOME}/.pulumi/bin:${env.PATH}"
-        PULUMI_ACCESS_TOKEN = credentials('pulumi_access_token')
-        AWS_ACCESS_KEY_ID = credentials('aws-master-key')
-        AWS_SECRET_ACCESS_KEY = credentials('aws-master-secret')
         CICD = '1'
     }
 
