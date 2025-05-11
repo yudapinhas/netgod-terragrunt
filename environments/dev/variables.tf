2d0802cf6d75d59b96d91784ffdc2d7147afa1d7
= <yuda.pinhas@skai.io>
2025-05-11 19:32:20 +0300

    tf/simple_s3

---
+++ environments/dev/variables.tf
@@ -0,0 +1,9 @@
+variable "bucket_name" {
+  type        = string
+  description = "Bucket name to create"
+}
+
+variable "environment" {
+  type        = string
+  description = "Environment name (e.g. dev, prod)"
+}
