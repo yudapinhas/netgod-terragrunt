6fc28e3f6e9f016a8b639c2fe54be5c913d1070d
NetGod <yuda.pinhas@gmail.com>
2025-05-24 17:41:21 +0200

    Update pull_request.groovy

--- buildScripts/jenkins/pipelines/pull_request.groovy
+++ buildScripts/jenkins/pipelines/pull_request.groovy
@@ -65,14 +65,14 @@ pipeline {
         
         stage('Terraform Plan') {
             steps {
-                withCredentials([string(credentialsId: 'terraform-cloud-token', variable: 'RAW_TF_TOKEN')]) {
-                    withEnv(['TF_TOKEN_app_terraform_io=$RAW_TF_TOKEN']) {
-                        sh '''
-                            set -eux
-                            terraform init
-                            terraform plan
-                        '''
-                    }
+                withCredentials([
+                  string(credentialsId: 'terraform-cloud-token', variable: 'TF_TOKEN_app_terraform_io')
+                ]) {
+                    sh '''
+                        set -eux
+                        terraform init
+                        terraform plan
+                    '''
                 }
             }
         }
