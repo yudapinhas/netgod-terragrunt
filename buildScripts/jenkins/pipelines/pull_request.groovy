4168f292cd457f9541d5d638f3201c3564e149a9
NetGod <yuda.pinhas@gmail.com>
2025-06-06 21:59:10 +0300

    Update pull_request.groovy

--- buildScripts/jenkins/pipelines/pull_request.groovy
+++ buildScripts/jenkins/pipelines/pull_request.groovy
@@ -1,8 +1,6 @@
 @Library('netgod-jenkins-shared-lib@master') _
-
 pipeline {
     agent any
-
     environment {
         CICD      = '1'
         TOOL_DIR  = '/var/jenkins_home/tools/bin'
@@ -11,13 +9,11 @@ pipeline {
         ORG       = 'yudapinhas'
         REPO_URL  = "git@github.com:${ORG}/${REPO_NAME}.git"
     }
-
     options {
         ansiColor('xterm')
         skipDefaultCheckout()
         buildDiscarder(logRotator(numToKeepStr: '10'))
     }
-
     stages {
         stage('Checkout PR branch') {
             steps {
@@ -31,7 +27,6 @@ pipeline {
                 ])
             }
         }
-
         stage('Determine Terragrunt Targets') {
             steps {
                 script {
@@ -39,34 +34,36 @@ pipeline {
                         script: 'find environments -type f -name "terragrunt.hcl" -exec dirname {} \\; | sort -u',
                         returnStdout: true
                     ).trim().split('\n').findAll { it }
-
                     if (modules.isEmpty()) {
                         echo "No terragrunt.hcl files found in environments/. Proceeding with no targets."
                         env.TG_CHANGED_PATHS = ""
                     } else {
                         env.TG_CHANGED_PATHS = modules.join(',')
-                        echo "Detected Terragrunt modules:\n${env.TG_CHANGED_PATHS}"
+                        echo "Detected Terragrunt modules:\n${env.TG_CHANGED_PATHS.replace(',', '\n')}"
                     }
                 }
             }
         }
-
         stage('Terragrunt Plan') {
             when {
                 expression { env.TG_CHANGED_PATHS != "" }
             }
             steps {
-                withCredentials([file(credentialsId: 'gcp-sa-json', variable: 'GCP_KEY')]) {
+                withCredentials([
+                    file(credentialsId: 'gcp-sa-json', variable: 'GCP_KEY'),
+                    string(credentialsId: 'terraform-cloud-token', variable: 'TF_TOKEN_app_terraform_io')
+                ]) {
                     script {
                         env.GCP_CREDENTIALS_PATH = "gcp/credentials.json"
-                        sh "mkdir -p gcp && cp $GCP_KEY $GCP_CREDENTIALS_PATH"
-
+                        sh "mkdir -p gcp && cp \$GCP_KEY \$GCP_CREDENTIALS_PATH"
+                        
                         def modules = env.TG_CHANGED_PATHS.split(',')
                         modules.each { modulePath ->
                             echo "Running Terragrunt plan in ${modulePath}"
                             dir(modulePath) {
                                 sh '''
                                     set -eux
+                                    echo "Terraform Cloud token configured: $(echo $TF_TOKEN_app_terraform_io | cut -c1-10)..."
                                     terragrunt init
                                     terragrunt plan
                                 '''
@@ -77,7 +74,6 @@ pipeline {
             }
         }
     }
-
     post {
         cleanup {
             cleanWs()
